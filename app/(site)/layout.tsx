a9c6df62dbad8f22738b72537fd30e340080c8a2
Joowon Hyun <lkk7913@naver.com>
2024-04-20 23:23:30 +0900

    feat: add HeaderCompoent

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,8 +1,12 @@
 import { ComponentProps } from "@/components/type";
 import React from "react";
-
+import Header from "@/components/Header";
 const layout = ({ children }: ComponentProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="w-full h-full">
+      <Header>{children}</Header>
+    </div>
+  );
 };
 
 export default layout;
