786d43af8c41063181e810dda45435de0c6fd5e9
Joowon Hyun <lkk7913@naver.com>
2024-07-24 00:04:23 +0900

    feat: 메인 페이지 구성

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import Category from "@/app/(site)/components/Category";
 import PagePadding from "@/components/PagePadding";
 import PlayListCarousel from "@/components/PlayListCarousel";
-import { dummyPlaylistArray } from "@/lib/dummyData";
+import { dummyPlaylistArray, getPlaylistById } from "@/lib/dummyData";
 import UserIcon from "@/components/UserIcon";
 
 const page = async () => {
+  const dummyPlaylistArray1 = [...dummyPlaylistArray];
+  const dummyPlaylistArray2 = [await getPlaylistById(1)];
+  const dummyPlaylistArray3 = [await getPlaylistById(2)];
+  const dummyPlaylistArray4 = [await getPlaylistById(3)];
+
   return (
     <PagePadding>
       <div className="min-h-[600px]">
@@ -13,13 +18,30 @@ const page = async () => {
         <Category />
         <div className="mt-12"></div>
         <PlayListCarousel
-          playListArray={[...dummyPlaylistArray]}
+          playListArray={[...dummyPlaylistArray1]}
           Thumbnail={
             <div className="w-[56px] h-[56px]">
               <UserIcon size={"lg"} />
             </div>
           }
           title="다시 듣기"
+          subTitle="주원"
+        />
+        <div className="mt-20"></div>
+        <PlayListCarousel
+          playListArray={[...dummyPlaylistArray2]}
+          title="케이시 - Full Bloom"
+          subTitle="새로운 앨범"
+        />
+        <div className="mt-20"></div>
+        <PlayListCarousel
+          playListArray={[...dummyPlaylistArray3]}
+          title="커뮤니티 제공"
+        />
+        <div className="mt-20"></div>
+        <PlayListCarousel
+          playListArray={[...dummyPlaylistArray4]}
+          title="커버 및 리믹스"
           subTitle="주원"
         />
       </div>
