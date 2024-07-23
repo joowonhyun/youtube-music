7a8ddb412995ff7808baa6667ba8eb2ec76a227c
Joowon Hyun <lkk7913@naver.com>
2024-07-24 00:28:10 +0900

    design: 공통 컴포넌트 헤더 적용

--- app/playlist/layout.tsx
+++ app/playlist/layout.tsx
@@ -1,8 +1,13 @@
 import { ComponentProps } from "@/types";
 import React from "react";
+import Header from "@/components/Header";
 
 const layout = ({ children }: ComponentProps) => {
-  return <div>{children}</div>;
+  return (
+    <div className="w-full h-full">
+      <Header>{children}</Header>
+    </div>
+  );
 };
 
 export default layout;
