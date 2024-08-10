c17f26735c19560a5810ee6bf5016e5b4cc29b16
joowonhyun <78545341+joowonhyun@users.noreply.github.com>
2024-08-10 15:52:34 +0900

    feat: songlist 컴포넌트 반영

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -7,15 +7,25 @@ import {
   getSongListTop10,
 } from "@/lib/dummyData";
 import PlayListCarousel from "@/components/PlayListCarousel";
+import SongListCarousel from "@/components/SongListCarousel";
 
 const page = async () => {
-  const playListArray = await getAllPlaylist();
+  const [playListArray, songListTop10] = await Promise.all([
+    getAllPlaylist(),
+    getSongListTop10(),
+  ]);
+
   return (
     <PagePadding>
       <div className="mt-4"></div>
       <Category />
       <div className="mt-20"></div>
       <PlayListCarousel playListArray={playListArray} title="새 앨범 및 싱글" />
+      <div className="mt-20"></div>
+      <SongListCarousel songListTop10={songListTop10} title="인기곡" />
+      <div className="mt-20"></div>
+      <div className="mt-20"></div>
+      <div className="mt-20"></div>
     </PagePadding>
   );
 };
