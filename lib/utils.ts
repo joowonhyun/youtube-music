93bf63e10a841d8513a580b40af3ecadb6e1fc37
joowonhyun <78545341+joowonhyun@users.noreply.github.com>
2024-08-10 15:47:56 +0900

    remove: 불필요 코드 삭제

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
-// await sleep(1000)
-
 export function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
